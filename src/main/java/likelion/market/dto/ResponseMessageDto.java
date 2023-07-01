08f5421148b78bb952086a75b1e3529ec7cbc5d9
Shjung <wjdtngus9@gmail.com>
2023-07-01 21:41:09 +0900

    Feat : ResponseMessageDto 추가
    요청에 대한 응답 메세지를 반환하는 클래스

---
+++ src/main/java/likelion/market/dto/ResponseMessageDto.java
@@ -0,0 +1,13 @@
+/**
+ * @project MiniProject_Basic_JungSyHyeon
+ * @since 2023-06-29
+ */
+
+package likelion.market.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseMessageDto {
+    private String message;
+}
