8278302743b3024341146b6dddef60f4c13e19b3
Shjung <wjdtngus9@gmail.com>
2023-07-03 18:07:11 +0900

    Feat : 연관관계 추가 (SalesItemEntity 클래스)

--- src/main/java/likelion/market/entity/CommentEntity.java
+++ src/main/java/likelion/market/entity/CommentEntity.java
@@ -1,9 +1,6 @@
 package likelion.market.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +8,12 @@ import lombok.Setter;
 @Setter
 @Entity
 public class CommentEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Integer ItemId; // sales_item id
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private SalesItemEntity itemId; // sales_item id
     private String writer;
     private String password;
     private String content;
