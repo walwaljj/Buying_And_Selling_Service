f3109be8991935c88f28bfe781cd85241977e7be
Shjung <wjdtngus9@gmail.com>
2023-07-05 18:37:05 +0900

    Feat : NegotiationEntity 에 연관관계 추가

--- src/main/java/likelion/market/entity/NegotiationEntity.java
+++ src/main/java/likelion/market/entity/NegotiationEntity.java
@@ -1,23 +1,34 @@
+/**
+ * @project MiniProject_Basic_JungSyHyeon
+ * @since 2023-07-04
+ */
+
 package likelion.market.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 @Setter
 @Entity
 public class NegotiationEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Integer ItemId; // sales_item id
-    private int suggested_price;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    @JsonBackReference
+    private SalesItemEntity itemId; // sales_item id
+    @Column(name = "suggested_price")
+    private int suggestedPrice;
+
     private String status;
+    @NotNull
     private String writer;
+    @NotNull
     private String password;
 }
